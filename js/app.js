1d4648713e263e2c22dfa1f357c483f22c8202bb
deanjheikkila <deanjheikkila@gmail.com>
2020-02-07 15:14:58 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// DOM references
+let numDisplay = document.getElementById("numberDisplay");
+let plusButton = document.querySelectorAll("button")[0];
+let minusButton = document.querySelectorAll("button")[1];
+let inputBox = document.querySelector("input");
+
+// variables
+let numDisplayed = 0;
+
+plusButton.addEventListener("click", function(e){
+    numDisplayed += parseInt(inputBox.value, 10);
+    console.log(numDisplay.innerText, numDisplay);
+    numDisplay.innerText = numDisplayed;
+    if (numDisplayed >= 0){
+        numDisplay.style.color = "black";
+    }
+});
+minusButton.addEventListener("click", function(e){
+    numDisplayed -= inputBox.value;
+    numDisplay.innerText = numDisplayed;
+    if (numDisplayed < 0){
+        numDisplay.style.color = "red";
+    }
+});
